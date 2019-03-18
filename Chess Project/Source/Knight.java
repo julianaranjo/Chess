371f0bf5ba6fbae55b732f7714ac6a230de972f6
Matt Jones <jonemath@mail.gvsu.edu>
2019-03-18 15:14:49 -0400

    Update Knight.java

--- Chess Project/Source/Knight.java
+++ Chess Project/Source/Knight.java
@@ -1,21 +1,70 @@
-package Source;
-
 public class Knight extends ChessPiece {
 
-	public Knight(Player player) {
-		super(player);
-	}
+    public Knight(Player player) {
+        super(player);
+    }
 
-	public String type() {
-		return "Knight";
-	}
+    public String type() {
+        return "Knight";
+    }
 
-	public boolean isValidMove(Move move, IChessPiece[][] board){
+    public boolean isValidMove(Move move, IChessPiece[][] board) {
 
-		boolean valid = true;
-        // More code is needed
-		return valid;
-		
-	}
+        boolean valid = false;
+
+        if (move.toColumn > board.length || move.toRow > board.length) {
+            valid = false;
+        }
+
+        if (move.toColumn < 0 || move.toRow < 0) {
+            valid = false;
+        }
+
+        if (move.toColumn == move.fromColumn + 0 || move.toRow == move.fromRow + 0) {
+            valid = false;
+        }
+
+        if (move.toColumn == move.fromColumn + 1) {
+            if (move.toRow == move.fromRow + 2) {
+                valid = true;
+            }
+
+            else if (move.toRow == move.fromRow - 2) {
+                valid = true;
+            }
+        }
+
+        if (move.toColumn == move.fromColumn - 1) {
+            if (move.toRow == move.fromRow + 2) {
+                valid = true;
+            }
+
+            else if (move.toRow == move.fromRow - 2) {
+                valid = true;
+            }
+        }
+
+        if (move.toColumn == move.fromColumn + 2) {
+            if (move.toRow == move.fromRow + 1) {
+                valid = true;
+            }
+
+            else if (move.toRow == move.fromRow - 1) {
+                valid = true;
+            }
+        }
+
+        if (move.toColumn == move.fromColumn - 2) {
+            if (move.toRow == move.fromRow + 1) {
+                valid = true;
+            }
+
+            else if (move.toRow == move.fromRow - 1) {
+                valid = true;
+            }
+        }
+
+        return valid;
+    }
 
 }
