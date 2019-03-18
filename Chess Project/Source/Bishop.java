426fdd04c35d12416652e571dd730507ab0fd7ef
Matt Jones <jonemath@mail.gvsu.edu>
2019-03-18 15:15:33 -0400

    Update Bishop.java

--- Chess Project/Source/Bishop.java
+++ Chess Project/Source/Bishop.java
@@ -1,19 +1,48 @@
-package Source;
-
 public class Bishop extends ChessPiece {
 
-	public Bishop(Player player) {
-		super(player);
-	}
+    public Bishop(Player player) {
+        super(player);
+    }
 
-	public String type() {
-		return "Bishop";
-	}
-	
-	public boolean isValidMove(Move move, IChessPiece[][] board) {
+    public String type() {
+        return "Bishop";
+    }
 
-		return true;
-        // More code is needed
-		
-	}
+    public boolean isValidMove(Move move, IChessPiece[][] board) {
+
+        boolean valid = false;
+
+        if (move.toColumn > board.length || move.toRow > board.length) {
+            valid = false;
+        }
+
+        if (move.toColumn < 0 || move.toRow < 0) {
+            valid = false;
+        }
+
+        if (move.toColumn == move.fromColumn + 0 || move.toRow == move.fromRow + 0) {
+                    valid = false;
+                }
+        for (int i = 1; i < board.length; i++){
+            if(move.toColumn == move.fromColumn + i && move.toRow == move.fromRow + i){
+                valid = true;
+            }
+
+            else if(move.toColumn == move.fromColumn - i && move.toRow == move.fromRow + i){
+                valid = true;
+            }
+
+            else if(move.toColumn == move.fromColumn - i && move.toRow == move.fromRow - i){
+                valid = true;
+            }
+
+            else if(move.toColumn == move.fromColumn + i && move.toRow == move.fromRow - i){
+                valid = true;
+            }
+
+        }
+        
+        return valid;
+    }
+
 }
