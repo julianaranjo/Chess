ffa3a05549109655432f3ba57d168e7bfe7378d7
julianaranjo <48454286+julianaranjo@users.noreply.github.com>
2019-03-26 07:38:58 -0400

    Update Rook.java

--- Chess Project/Source/Rook.java
+++ Chess Project/Source/Rook.java
@@ -38,19 +38,50 @@ public class Rook extends ChessPiece {
 	 * @param move the move being validated
 	 * @param board the chess board array
 	 ******************************************************************/
-	public boolean isValidMove(Move move, IChessPiece[][] board) {
-		if ((move.fromRow == move.toRow) && (move.fromColumn == move.toColumn))
-			return false;
+	public boolean isValidMove(Move move, IChessPiece[][] board)
+	{
+		int ra = move.fromRow, rb = move.toRow;
+		int ca = move.fromColumn, cb = move.toColumn;
 
-		if (board[move.toRow][move.toColumn] != null)
-			if (board[move.toRow][move.toColumn].player().equals(player()))
+		// cannot move to the same square
+		if (ra==rb && ca==cb)
+		{
+			return false;
+		}
+
+		// has to move to same row or column
+		if (rb!=ra && cb!=ca)
+		{
+			return false;
+		}
+
+		// target square cannot contain piece of the same color
+		if (board[rb][cb] != null && board[ra][ca].player()==board[rb][cb].player())
+		{
+			return false;
+		}
+
+		// for example if rook is moving up-right, then dr=-1 and dc=1 (there are four possibilies)
+		int dr = 0;
+		if (rb-ra!=0)
+			dr = (rb-ra)/Math.abs(rb-ra);
+		int dc = 0;
+		if (cb-ca!=0)
+			dc = (cb-ca)/Math.abs(cb-ca);
+
+		int r = ra+dr;
+		int c = ca+dc;
+		while (r!=rb || c!=cb)
+		{
+			// if a space strictly between the current space and target space is occupied
+			if (board[r][c]!=null)
 				return false;
 
-		boolean valid = true;
-		if (move.fromColumn != move.toColumn && move.fromRow != move.toRow) {
-			valid = false;
+			// next space
+			r += dr;
+			c += dc;
 		}
-		return valid;
 
+		return true;
 	}
 }
